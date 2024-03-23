2d4c0d229d7bf8f2b7f56ba4613f8801f99a3b5b
tormeyjustin <152330875+tormeyjustin@users.noreply.github.com>
2024-03-23 21:03:02 +0000

    Added ReportOutput class that implements ReportFormatter interface

---
+++ src/ca2/ReportOutput.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Justin
+ * 
+ */
+
+public class ReportOutput implements Interfaces.ReportFormatter {
+
+    @Override
+    public void txtOutput(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void csvOutput(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void consoleOutput(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
