d0ed9ae40f7150ffb1d68682aa3ff34685ca5110
tormeyjustin <152330875+tormeyjustin@users.noreply.github.com>
2024-03-23 14:45:25 +0000

    Added DBConnector Class

---
+++ src/ca2/DbConnector.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2;
+
+
+/**
+ *
+ * @author Justin
+ * 
+ */
+
+public class DbConnector implements Interfaces.DatabaseAccess {
+    // DB credentials
+    private final String DB_URL = "jdbc:mysql://localhost/collegelms";
+    private final String USER = "pooa2024";
+    private final String PASSWORD = "pooa2024";
+    
+
+    @Override
+    public void connect() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void executeQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void disconnect() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
