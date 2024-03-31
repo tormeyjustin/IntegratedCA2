7b595a0fc36ec1d6e44fe389488b0ec37e4a01dc
tormeyjustin <152330875+tormeyjustin@users.noreply.github.com>
2024-03-31 16:14:42 +0100

    Added lecturer class

--- src/CA2/Lecturer.java
+++ src/CA2/Lecturer.java
@@ -4,10 +4,60 @@
  */
 package CA2;
 
+import java.util.List;
+
 /**
  *
  * @author Justin
+ * 
  */
+
 public class Lecturer {
+    private String firstName;
+    private String lastName;
+    private String role;
+    private List modulesTeaching;
+    private List skills;
+    
+    public Lecturer (String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @return the modulesTeaching
+     */
+    public List getModulesTeaching() {
+        return modulesTeaching;
+    }
+
+    /**
+     * @return the skills
+     */
+    public List getSkills() {
+        return skills;
+    }
     
 }
